1330e69ed0311dba8d42c3a3e0ea36d501c7ea2a
Zoe Stuart <zoestuart13@gmail.com>
2017-04-21 15:51:44 -0400

    Add files via upload

--- game.py
+++ game.py
@@ -18,14 +18,20 @@ font_md = pygame.font.Font(None, 64)
 font_lg = pygame.font.Font(None, 72)
 
 # Images
-hero_img_right = pygame.image.load("assets/harry_adorable_right.png")
+hero_img_right = pygame.image.load("assets/harry/harry_right.png")
 hero_img_right = pygame.transform.scale(hero_img_right, (64, 64))
-hero_img_left = pygame.image.load("assets/harry_adorable_left.png")
+hero_img_left = pygame.image.load("assets/harry/harry_left.png")
 hero_img_left = pygame.transform.scale(hero_img_left, (64, 64))
 hero_images = [hero_img_right, hero_img_left]
 
 block_img = pygame.image.load("assets/medievalTile_064.png")
 block_img = pygame.transform.scale(block_img, (64, 64))
+
+dementor_img = pygame.image.load("assets/dementor.png")
+dementor_img = pygame.transform.scale(dementor_img, (64, 64))
+
+life_img = pygame.image.load("assets/hedwig_down.png")
+life_img = pygame.transform.scale(life_img, (64, 64))
 
 diadem_img = pygame.image.load("assets/horcruxes/diadem.png")
 diadem_img = pygame.transform.scale(diadem_img, (32, 32))
@@ -56,7 +62,7 @@ JUMP = pygame.K_UP
 class Entity(pygame.sprite.Sprite):
 
     def __init__(self, x, y, image):
-        super(Entity, self).__init__()
+        super().__init__()
 
         self.image = pygame.Surface([64, 64], pygame.SRCALPHA, 32)
         self.image.blit(image, [0, 0])
@@ -69,23 +75,24 @@ class Entity(pygame.sprite.Sprite):
 class Block(Entity):
     
     def __init__(self, x, y, image):
-        super(Block, self).__init__(x, y, image)
+        super().__init__(x, y, image)
 
 
 class Character(Entity):
 
     def __init__(self, x, y, images):
-        super(Character, self).__init__(x, y, images[0])
+        super().__init__(x, y, images[0])
         
         self.images = images
 
-        self.speed = 10
-        self.jump_power = 20
+        self.speed = 7
+        self.jump_power = 18
         
         self.vx = 0
         self.vy = 0
         
         self.score = 0
+        self.lives = 3
 
     def apply_gravity(self, level):
         self.vy += level.gravity
@@ -123,7 +130,13 @@ class Character(Entity):
         hit_list = pygame.sprite.spritecollide(self, horcruxes, True)
         
         for horcrux in hit_list:
-            self.score += horcrux.value     
+            self.score += horcrux.value
+
+    def process_powerups(self, powerups):
+        hit_list = pygame.sprite.spritecollide(self, powerups, True)
+        
+        for powerup in hit_list:
+            powerup.apply(self)
     
     def move_left(self):
         self.vx = -1 * self.speed
@@ -151,28 +164,154 @@ class Character(Entity):
         self.apply_gravity(level)
         self.check_world_edges(level)
         self.process_blocks(level.blocks)
-        
         self.process_horcruxes(level.horcruxes)
+        self.process_powerups(level.powerups)
 
 
+class Dementor(Entity):
+    
+    def __init__(self, x, y, image):
+        super().__init__(x, y, image)
 
+        self.vx = -2
+        self.vy = 0
+
+    def reverse_direction(self):
+        self.vx *= -1
+        
+    def apply_gravity(self, level):
+        self.vy += level.gravity
+
+    def check_world_edges(self, level):
+        if self.rect.left < 0:
+            self.rect.left = 0
+            self.reverse_direction()
+        elif self.rect.right > level.width:
+            self.rect.right = level.width
+            self.reverse_direction()
+
+    def process_blocks(self, blocks):
+        self.rect.x += self.vx
+        hit_list = pygame.sprite.spritecollide(self, blocks, False)
+
+        for block in hit_list:
+            if self.vx > 0:
+                self.rect.right = block.rect.left
+                self.reverse_direction()
+            elif self.vx < 0:
+                self.rect.left = block.rect.right
+                self.reverse_direction()
+
+        self.rect.y += self.vy
+        hit_list = pygame.sprite.spritecollide(self, blocks, False)
+
+        for block in hit_list:
+            if self.vy > 0:
+                self.rect.bottom = block.rect.top
+                self.vy = 0
+            elif self.vy < 0:
+                self.rect.top = block.rect.bottom
+                self.vy = 0
+
+            
+    def update(self, level):
+        self.apply_gravity(level)
+        self.check_world_edges(level)
+        self.process_blocks(level.blocks)
+
+
+class Pixie(Entity):
+    
+    def __init__(self, x, y, image):
+        super().__init__(x, y, image)
+
+        self.vx = -2
+        self.vy = 0
+
+    def reverse_direction(self):
+        self.vx *= -1
+        
+    def apply_gravity(self, level):
+        self.vy += level.gravity
+
+    def check_world_edges(self, level):
+        if self.rect.left < 0:
+            self.rect.left = 0
+            self.reverse_direction()
+        elif self.rect.right > level.width:
+            self.rect.right = level.width
+            self.reverse_direction()
+
+    def process_blocks(self, blocks):
+        self.rect.x += self.vx
+        hit_list = pygame.sprite.spritecollide(self, blocks, False)
+
+        for block in hit_list:
+            if self.vx > 0:
+                self.rect.right = block.rect.left
+                self.reverse_direction()
+            elif self.vx < 0:
+                self.rect.left = block.rect.right
+                self.reverse_direction()
+
+        at_edge = True
+        self.rect.y += self.vy
+        hit_list = pygame.sprite.spritecollide(self, blocks, False)
+
+        for block in hit_list:
+            if self.vy >= 0:
+                self.rect.bottom = block.rect.top
+                self.vy = 0
+
+                if self.vx < 0:
+                    if block.rect.left <= self.rect.left:
+                        at_edge = False
+
+                if self.vx > 0:
+                    if block.rect.right >= self.rect.right:
+                        at_edge = False
+                
+            elif self.vy < 0:
+                self.rect.top = block.rect.bottom
+                self.vy = 0
+
+        if at_edge:
+            self.reverse_direction()
+            
+    def update(self, level):
+        self.apply_gravity(level)
+        self.check_world_edges(level)
+        self.process_blocks(level.blocks)
+
+        
 class Horcrux(Entity):
     
     def __init__(self, x, y, image):
-        super(Horcrux, self).__init__(x, y, image)
+        super().__init__(x, y, image)
         
         self.value = 1
 
-class Enemy():
-    pass
+        
+class Life(Entity):
+    
+    def __init__(self, x, y, image):
+        super().__init__(x, y, image)
 
+    def apply(self, character):
+        character.lives += 1
+        print(character.lives)
+
+
+        
 class Level():
-    def __init__(self, blocks, horcruxes):
+    def __init__(self, blocks, horcruxes, enemies, powerups):
         self.blocks = blocks
         self.horcruxes = horcruxes
-        
+        self.enemies = enemies
+        self.powerups = powerups
+
         self.active_sprites = pygame.sprite.Group()
-        self.active_sprites.add(self.horcruxes)
+        self.active_sprites.add(self.horcruxes, self.enemies, self.powerups)
         
         self.inactive_sprites = pygame.sprite.Group()
         self.inactive_sprites.add(blocks)
@@ -201,7 +340,7 @@ class Game():
         self.active_layer = pygame.Surface([level.width, level.height], pygame.SRCALPHA, 32)
         self.inactive_layer = pygame.Surface([level.width, level.height], pygame.SRCALPHA, 32)
 
-        for i in range(0, level.width, background_img.get_width()):
+        for i in range(0, level.width * 2, background_img.get_width()):
             self.background_layer.blit(background_img, [i, 0])
             
         self.level.inactive_sprites.draw(self.inactive_layer)
@@ -241,6 +380,7 @@ class Game():
                 self.hero.stop()
 
             self.hero.update(self.level)
+            self.level.enemies.update(self.level)
             
             #Drawing
 
@@ -275,16 +415,25 @@ def main():
     blocks.add(Block(192, 448, block_img))
     blocks.add(Block(256, 448, block_img))
     blocks.add(Block(320, 448, block_img))
-
+    
     blocks.add(Block(448, 320, block_img))
     blocks.add(Block(512, 320, block_img))
-
+    
+    blocks.add(Block(768, 512, block_img))
+    
     horcruxes = pygame.sprite.Group()
-    horcruxes.add(Horcrux(768, 416, diadem_img))
-    horcruxes.add(Horcrux(256, 352, ring_img))
+    horcruxes.add(Horcrux(768, 448, diadem_img))
+    horcruxes.add(Horcrux(256, 384, ring_img))
+
+    enemies = pygame.sprite.Group()
+    enemies.add(Dementor(640, 512, dementor_img))
+    enemies.add(Pixie(320, 384, dementor_img))
+
+    powerups = pygame.sprite.Group()
+    powerups.add(Life(192, 192, life_img))
 
     # Make a level
-    level = Level(blocks, horcruxes)
+    level = Level(blocks, horcruxes, enemies, powerups)
 
     # Start game
     game = Game(hero)
